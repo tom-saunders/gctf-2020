ca138ab41cd096f489a60b54464b4dcee6b2a8fb
tom saunders <github@paradoxic.al>
2020-08-22 21:26:27 +0000

    basic hw task complte

--- hw/basic/check.sv
+++ hw/basic/check.sv
@@ -2,7 +2,11 @@ module check(
     input clk,
 
     input [6:0] data,
-    output wire open_safe
+    output wire open_safe,
+    output wire [63:0] meut,
+    output wire [63:0] maut,
+    output wire [63:0] kout,
+    output wire [63:0] exp
 );
 
 reg [6:0] memory [7:0];
@@ -15,8 +19,52 @@ wire [55:0] magic = {
     {memory[7], memory[1]}
 };
 
+
 wire [55:0] kittens = { magic[9:0],  magic[41:22], magic[21:10], magic[55:42] };
 assign open_safe = kittens == 56'd3008192072309708;
+
+wire [63:0] meut = {
+    1'0, memory[7],
+    1'0, memory[6],
+    1'0, memory[5],
+    1'0, memory[4],
+    1'0, memory[3],
+    1'0, memory[2],
+    1'0, memory[1],
+    1'0, memory[0]
+};
+wire [63:0] maut = {
+    1'0, magic[55:49],
+    1'0, magic[48:42],
+    1'0, magic[41:35],
+    1'0, magic[34:28],
+    1'0, magic[27:21],
+    1'0, magic[20:14],
+    1'0, magic[13:7],
+    1'0, magic[6:0]
+};
+wire [63:0] kout = {
+    1'0, kittens[55:49],
+    1'0, kittens[48:42],
+    1'0, kittens[41:35],
+    1'0, kittens[34:28],
+    1'0, kittens[27:21],
+    1'0, kittens[20:14],
+    1'0, kittens[13:7],
+    1'0, kittens[6:0]
+};
+
+reg [55:0] act_exp = 56'd3008192072309708;
+wire [63:0] exp = {
+    1'0, act_exp[55:49],
+    1'0, act_exp[48:42],
+    1'0, act_exp[41:35],
+    1'0, act_exp[34:28],
+    1'0, act_exp[27:21],
+    1'0, act_exp[20:14],
+    1'0, act_exp[13:7],
+    1'0, act_exp[6:0]
+};
 
 always_ff @(posedge clk) begin
     memory[idx] <= data;
--- hw/basic/main.cpp
+++ hw/basic/main.cpp
@@ -1,12 +1,14 @@
 #include "obj_dir/Vcheck.h"
 
 #include <iostream>
+#include <iomanip>
 #include <memory>
 
 int main(int argc, char *argv[]) {
     Verilated::commandArgs(argc, argv);
     std::cout << "Enter password:" << std::endl;
     auto check = std::make_unique<Vcheck>();
+
 
     for (int i = 0; i < 100 && !check->open_safe; i++) {
         int c = fgetc(stdin);
@@ -16,7 +18,9 @@ int main(int argc, char *argv[]) {
         check->eval();
         check->clk = true;
         check->eval();
+        std::cout << std::setw(16) << std::setfill('0') << std::hex << check->kout <<  ' ' << c <<std::endl;
     }
+        std::cout << std::setw(16) << std::setfill('0') << std::hex << check->exp << std::endl;
     if (check->open_safe) {
         std::cout << "CTF{real flag would be here}" << std::endl;
     } else {
