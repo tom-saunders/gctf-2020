86a3d5ab26eb2423ba84c0ff5f0463abff63b295
Debian <admin@ip-172-31-37-2.ec2.internal>
2020-08-22 13:13:34 +0000

    some more 'helpful' things

--- rev/beginner/intercept.c
+++ rev/beginner/intercept.c
@@ -2,13 +2,35 @@
 #include <stdio.h>
 
 int strncmp(char const *s1 , char const *s2, size_t n) {
-    char b1[128];
-    char b2[128];
-    int ls1 = strnlen(s1, n);
-    int ls2 = strnlen(s2, n);
-    strncpy(b1, s1, ls1 <= 127 ? ls1 : 127);
-    strncpy(b2, s2, ls2 <= 127 ? ls2 : 127);
+    unsigned char b1[17];
+    unsigned char b2[17];
+    unsigned int ls1 = strnlen(s1, n);
+    unsigned int ls2 = strnlen(s2, n);
+    strncpy(b1, s1, 16);
+    b1[16] = '\0';
+    strncpy(b2, s2, 16);
+    b2[16] = '\0';
     int r = strcmp(b1, b2);
-    printf("\nstrncmp\n[%lu] [%d] [%d]\n[%s]\n[%s]\n[%d]\n", n, ls1, ls2, b1, b2, r);
+    printf("\n[0x");
+    fflush(0);
+    for (int i = 0; i < 16; ++i) {
+        printf("%02x", (unsigned int) b1[i]);
+        if ((i % 4) == 3) {
+            printf(" ");
+        }
+        fflush(0);
+    }
+    printf("]\n");
+    printf("[0x");
+    fflush(0);
+    for (int i = 0; i < 16; ++i) {
+        printf("%02x", (unsigned int) b2[i]);
+        if ((i % 4) == 3) {
+            printf(" ");
+        }
+        fflush(0);
+    }
+    printf("] ");
+    printf("[%d]\n", r);
     return r;
 }
