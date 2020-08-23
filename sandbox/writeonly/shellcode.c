0e8d83c2605bcc183c853668e4c8491eb0be28ab
tom saunders <github@paradoxic.al>
2020-08-23 11:35:37 +0000

    Not currently working but convinient

--- sandbox/writeonly/shellcode.c
+++ sandbox/writeonly/shellcode.c
@@ -3,11 +3,16 @@
 #include <syscall.h>
 #include <sys/stat.h>
 void sc() {
-    uint32_t z = 0;
-    uint32_t x = 16;
+    uint32_t zero = 0;
+    uint32_t four = 4;
+    uint32_t sixteen = 16;
 
-    uint32_t r = 0;
-    uint32_t d = 1;
+    uint32_t ret = 0;
+    uint32_t fd_1 = 1;
+    uint32_t pid_2 = 2;
+    uint32_t err = 0;
+
+    uint32_t nocare = 0;
 
     struct stat stat_out;
 
@@ -20,20 +25,56 @@ void sc() {
       :
       : "rax", "memory");
     asm volatile (
+      "mov %%esi, %%esp\n\t"
+      "syscall"
+      : "=a"(ret), "=d"(err)
+      : "a"(__NR_write), "D"(fd_1), "d"(sixteen)
+      : "esi", "memory");
+    asm volatile (
       "mov %%edi, %%esp\n\t"
       "syscall"
-      : "=a"(r)
-      : "a"(__NR_open), "S"(z), "d"(z)
+      : "=a"(ret), "=d"(err)
+      : "a"(__NR_open), "S"(zero), "d"(zero)
       : "edi", "memory");
     asm volatile (
       "syscall"
-      : "=a"(r)
-      : "a"(__NR_fstat), "D"(r), "S"(&stat_out)
+      : "=a"(nocare)
+      : "a"(__NR_write), "D"(fd_1), "S"(&ret), "d"(four)
       : "memory");
     asm volatile (
       "syscall"
-      : "=a"(r)
-      : "a"(__NR_write), "D"(d), "S"(&stat_out), "d"(sizeof(struct stat))
+      : "=a"(nocare)
+      : "a"(__NR_write), "D"(fd_1), "S"(&err), "d"(four)
+      : "memory");
+    asm volatile (
+      "syscall"
+      : "=a"(ret), "=d"(err)
+      : "a"(__NR_fstat), "D"(ret), "S"(&stat_out)
+      : "memory");
+//    asm volatile (
+//      "syscall"
+//      : "=a"(r)
+//      : "a"(__NR_clone), "D"(0), "S"(0)
+//      : "memory");
+//    asm volatile (
+//      "syscall"
+//      :
+//      : "a"(__NR_write), "D"(fd_1), "S"(&err), "d"(4)
+//      : "memory");
+    asm volatile (
+      "syscall"
+      : "=a"(nocare)
+      : "a"(__NR_write), "D"(fd_1), "S"(&ret), "d"(4)
+      : "memory");
+    asm volatile (
+      "syscall"
+      : "=a"(nocare)
+      : "a"(__NR_write), "D"(fd_1), "S"(&err), "d"(4)
+      : "memory");
+    asm volatile (
+      "syscall"
+      : "=a"(ret)
+      : "a"(__NR_write), "D"(fd_1), "S"(&(stat_out.st_size)), "d"(sizeof(off_t))
       : "memory");
 }
 
